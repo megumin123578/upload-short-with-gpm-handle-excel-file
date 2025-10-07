79fef384ec23bb9e0adc111fff6d793cd30ec414
megumin123578 <saranghae123578@gmail.com>
2025-10-07 08:12:51 +0700

    update

--- src/hyperparameter.py
+++ src/hyperparameter.py
@@ -1,6 +1,6 @@
 import os
 
-APP_VERSION = "1.0.8"
+APP_VERSION = "1.0.9"
 UPDATE_MANIFEST = "https://raw.githubusercontent.com/megumin123578/upload-short-with-gpm-handle-excel-file/main/manifest.json"
 APP_TITLE = "AUTO GENERATE UPLOAD SHORT YOUTUBE"
 
