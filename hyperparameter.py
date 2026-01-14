eeda211bdd7d2e47fe835cae771706e1d5d0c03c
megumin12357 <saranghae123578@gmail.com>
2026-01-14 08:14:21 +0700

    update 1.0.79

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,6 +1,6 @@
 import os
 
-APP_VERSION = "1.0.78"
+APP_VERSION = "1.0.79"
 UPDATE_MANIFEST = "https://raw.githubusercontent.com/megumin123578/upload-short-with-gpm-handle-excel-file/main/manifest.json"
 APP_TITLE = f"AUTOMATION APP - {APP_VERSION}"
 
